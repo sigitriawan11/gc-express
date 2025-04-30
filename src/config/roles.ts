f4ed69d94b0e4e1605f04223904ada33d6c1e59c
Gitcode-11 <sigitnaxbhezie11@gmail.com>
2025-04-30 14:39:31 +0700

    clear file

--- src/config/roles.ts
+++ src/config/roles.ts
@@ -1,19 +1,5 @@
 export const roleCombinations = [
-  "roles:guest",
-  "roles:guest|member",
-  "roles:guest|member|super_admin",
-  "roles:guest|super_admin",
-  "roles:guest|super_admin|member",
-  "roles:member",
-  "roles:member|guest",
-  "roles:member|guest|super_admin",
-  "roles:member|super_admin",
-  "roles:member|super_admin|guest",
-  "roles:super_admin",
-  "roles:super_admin|guest",
-  "roles:super_admin|guest|member",
-  "roles:super_admin|member",
-  "roles:super_admin|member|guest"
+
 ] as const;
 
 export type RoleMiddleware = typeof roleCombinations[number];
